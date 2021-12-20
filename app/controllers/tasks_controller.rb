3e03220ca1de381bf2a8dcafb1e4b04b3fe26d1c
Ubuntu <ubuntu@ip-172-31-4-99.ap-northeast-1.compute.internal>
2021-12-20 04:19:30 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,15 @@
 class TasksController < ApplicationController
+  # 下記コードでこの中の全アクションがログイン必須
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   # tasks#index
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+      @tasks = Task.all
+    end
   end
   
   # tasks#show
@@ -16,16 +24,25 @@ class TasksController < ApplicationController
   
   # task#create
   def create
-    @task = Task.new(task_params)
-    
-    if @task.save
-      flash[:success] = "Task が正常に投稿されました"
-      redirect_to @task
-    else
-      flash.now[:danger] = "Task が投稿されませんでした"
-      render :new
-    end
+    @task = current_user.tasks.build(task_params)
+      if @task.save
+        flash[:success] = "メッセージを投稿しました。"
+        redirect_to root_url
+      else
+        @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+        flash.now[:danger] = "メッセージの投稿に失敗しました。"
+        render "tasks/index"
+      end
   end
+    # @task = Task.new(task_params)
+    
+    # if @task.save
+    #   flash[:success] = "Task が正常に投稿されました"
+    #   redirect_to @task
+    # else
+    #   flash.now[:danger] = "Task が投稿されませんでした"
+    #   render :new
+    # end
   
   # tasks#edit
   def edit
@@ -51,7 +68,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = "Task は正常に削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -59,5 +76,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task =current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
